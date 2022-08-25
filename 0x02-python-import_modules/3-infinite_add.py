aeea4ae4623b2c6ad481b23ce5415dcf5bdacc5c
Abraham <abrahamosazee3@gmail.com>
2022-08-25 19:50:43 +0000

    readme file

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,14 +1,18 @@
 #!/usr/bin/python3
 import sys
 
-count = 0
-for i in range(len(sys.argv)):
-    count += 1
-sum_val = 0
-idx = 1
-while count != 1:
-    sum_val += int(sys.argv[idx])
-    idx += 1
-    count -= 1
+
+def add_values():
+    count = 0
+    for i in range(len(sys.argv)):
+        count += 1
+    sum_val = 0
+    idx = 1
+    while count != 1:
+        sum_val += int(sys.argv[idx])
+        idx += 1
+        count -= 1
+
+
 if __name__ == "__main__":
     print(sum_val)
