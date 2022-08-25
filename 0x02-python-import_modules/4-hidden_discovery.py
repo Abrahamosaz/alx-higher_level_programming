96c09b00a1f884a4174a01867307c74bf46b62fe
Abraham <abrahamosazee3@gmail.com>
2022-08-25 22:19:05 +0000

    task 4

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-def discover():
+def discovr():
     name = dir(hidden_4)
     for i in name:
         if i[:2] != '--':
@@ -8,4 +8,4 @@ def discover():
 
 if __name__ == "__main__":
     import hidden_4
-    discover(hidden_4)
+    discovr(hidden_4)
