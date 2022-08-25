bbbbff147bb58e662bb139ee6afc68996591e4ad
Abraham <abrahamosazee3@gmail.com>
2022-08-25 20:15:48 +0000

    fix error

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python3
-def simple_cal(argv, exit, cal):
-    count = len(argv)
+def simple_cal(sys, cal):
+    count = len(sys.argv)
     if (count < 3):
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         sys.exit(1)
     else:
-        a = int(argv[1])
-        b = int(argv[3])
-        operator = argv[2]
+        a = int(sys.argv[1])
+        b = int(sys.argv[3])
+        operator = sys.argv[2]
         if (operator == '+'):
             print("{0:d} + {1:d} = {2:d}".format(a, b, cal.add(a, b)))
         elif (operator == '-'):
@@ -18,10 +18,10 @@ def simple_cal(argv, exit, cal):
             print("{0:d} / {1:d} = {2:d}".format(a, b, cal.div(a, b)))
         else:
             print("Unknown operator. Available operators: +, -, * and /")
-            exit(1)
+            sys.exit(1)
 
 
 if __name__ == "__main__":
     import sys
     import calculator_1 as cal
-    simple_cal(sys.argv, sys.exit, cal)
+    simple_cal(sys, cal)
