8eb6307469bfa31da2a300ae0e0131e7d56678b3
Abraham <abrahamosazee3@gmail.com>
2022-08-28 22:28:24 +0000

    fix check error

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
     for idx in matrix:
-        print(' '.join('{0:d}'.format(j) for j in idx))
+        print(' '.join("{:d}".format(j) for j in idx))
