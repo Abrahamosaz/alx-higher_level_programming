a0d13305bc2cad73a53930067389c6fd73a79efe
Abraham <abrahamosazee3@gmail.com>
2022-08-29 19:01:33 +0000

    efficient way of solving the task

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -1,34 +1,31 @@
 #include "lists.h"
-#define STACK_SIZE 100
 /**
- * is_palindrome -  check if the linked list forward transvers is the same when
- * back transvers backward
- * @head: address of the head pointer
+ * check_palindrome - check if the linked list is palindrome
+ * @head: address of the pointer to the start of the linked list
+ * @end: pointer to a listint_t
  *
- * Return: return 1 if it's palindrome and 0 otherwise
+ * Return: return 1 if its a palindrome and 0 otherwise
+ */
+int check_palindrome(listint_t **head, listint_t *end)
+{
+	if (!end)
+		return (1);
+	if (check_palindrome(head, end->next) && (*head)->n == end->n)
+	{
+		*head = (*head)->next;
+		return (1);
+	}
+	return (0);
+}
+/**
+ * is_palindrome - call the check_palindrome function
+ * @head: address of the pointer to ter start of the list
+ *
+ * Return: return 1 if True and 0 otherwise
  */
 int is_palindrome(listint_t **head)
 {
-	int stack[STACK_SIZE], count = 0;
-	listint_t *dir  = *head;
-
-	if (!head)
+	if (!head || !*head)
 		return (0);
-	while (dir)
-	{
-		stack[count] = dir->n;
-		count++;
-		dir = dir->next;
-	}
-	stack[count] = '\0';
-	dir = *head;
-	count--;
-	while (dir)
-	{
-		if (dir->n != stack[count])
-			return (0);
-		dir = dir->next;
-		count--;
-	}
-	return (1);
+	return (check_palindrome(head, *head));
 }
