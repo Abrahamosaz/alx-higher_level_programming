60c2a9c004bfbe349a4898190a6c91df6be36bf7
Abraham <abrahamosazee3@gmail.com>
2022-08-31 21:17:22 +0000

    advance tasks

---
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def weight_average(my_list=[]):
+    div_value = 0
+    sum_value = 0
+    for tuple_av in my_list:
+        div_value += tuple_av[1]
+        sum_value += (tuple_av[0] * tuple_av[1])
+    return (sum_value / div_value)
