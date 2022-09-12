401ce97015fb65425c869c9e97b12d5700b1af12
abraham <abrahamosazee3@gmail.com>
2022-09-12 20:26:06 +0100

    fix betty error

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -3,7 +3,9 @@
 def safe_print_division(a, b):
     try:
         result = a / b
+        print("Inside result: {0:.1f}".format(result))
     except ZeroDivisionError:
         result = None
+        print("Inside result: {}".format(result))
     finally:
-        print("Inside result: {0}".format(result))
+        return (result)
