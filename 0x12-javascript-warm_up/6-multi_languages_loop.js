7afb382eea83353bb121c7b0977380c7a91f7bee
Abraham <abrahamosazee3@gmail.com>
2023-03-13 19:14:14 +0100

    fix task 6 error

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 
-const stringArray = ['C is fun', 'Python is cool', 'javaScript is amazing'];
+const stringArray = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
 for (const x of stringArray) {
   console.log(x);
 }
