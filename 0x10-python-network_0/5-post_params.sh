d56f64a781cb594b0c79b9bbd9e9525984b41f12
Abraham <abrahamosazee3@gmail.com>
2023-03-30 21:27:08 +0100

    fix error in task 5

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # send a post request to the server
-curl -X POST -d '{"email": "test@gmail.com", "subject": "I will always be here for PLD"}' "$1"
+curl -X POST -d "email=test@gmail.com&subject=I will always be here for PLD" "$1"
